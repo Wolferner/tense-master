833491423b9f7a5838cb62ed2f792c899c23ba80
Maksim Sc <maksim.scerbuk@gmail.com>
2026-04-08 03:10:44 +0300

    feat: added old CORS check

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,21 @@
 import createMiddleware from 'next-intl/middleware';
-import type { NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server';
 import { routing } from './shared/i18n/config';
 
+const allowedOrigins = (process.env.ALLOWED_ORIGINS ?? '')
+	.split(';')
+	.map(o => o.trim())
+	.filter(Boolean);
 const handleI18nRouting = createMiddleware(routing);
 
 export function proxy(request: NextRequest) {
+	//FIXME: Need to implement CORS check
+	// const origin = request.headers.get('origin');
+
+	// if (origin && !allowedOrigins.includes(origin)) {
+	// 	return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+	// }
+
 	return handleI18nRouting(request);
 }
 
