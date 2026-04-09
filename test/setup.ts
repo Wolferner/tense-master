c049f52436b15df8ff7373f7fa272e07a5620561
Maksim Sc <maksim.scerbuk@gmail.com>
2026-04-10 01:54:08 +0300

    feat: added tests to LocaleSelect

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom';
+
+// Radix UI requires pointer/scroll APIs not available in jsdom
+Element.prototype.hasPointerCapture = () => false;
+Element.prototype.setPointerCapture = () => {};
+Element.prototype.releasePointerCapture = () => {};
+Element.prototype.scrollIntoView = () => {};
