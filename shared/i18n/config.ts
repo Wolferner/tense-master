d0f98a28594812e08a35b83d09821cafee6f9188
Maksim Sc <maksim.scerbuk@gmail.com>
2026-04-11 00:24:38 +0300

    feat: i18n config derives locales from domain Locale value object

--- shared/i18n/config.ts
+++ shared/i18n/config.ts
@@ -1,9 +1,10 @@
+import { Locale } from '@/domain/value-objects/Locale';
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-	locales: ['ru', 'fr', 'de', 'es'],
-	defaultLocale: 'ru',
+	locales: Object.values(Locale) as [Locale, ...Locale[]],
+	defaultLocale: Locale.ru,
 	// localeDetection: false,
 });
 
-export type Locale = (typeof routing.locales)[number];
+export type { Locale };
